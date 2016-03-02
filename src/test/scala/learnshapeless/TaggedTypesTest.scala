a164d4315c9f0160fcede557147e85beb99c27fb
Ben Hutchison <ben.hutchison@rea-group.com>
2016-03-02 15:43:28 +1100

    TaggedTypes, Coproduct modules

---
+++ src/test/scala/learnshapeless/TaggedTypesTest.scala
@@ -0,0 +1,12 @@
+package learnshapeless
+
+import learnshapeless.Data._
+import shapeless._
+
+class TaggedTypesTest extends org.specs2.mutable.Specification with Assert {
+
+  import TaggedTypes._
+
+  eg {mustEqual(ex_name, shapeless.tag[NameTag]("Katherine"))}
+
+}
