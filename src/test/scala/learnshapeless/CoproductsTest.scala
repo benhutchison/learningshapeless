a0b3c6670e476c6f090a19c7cdb31fe425ab5144
Ben Hutchison <ben.hutchison@rea-group.com>
2016-03-03 13:33:17 +1100

    corproduct

--- src/test/scala/learnshapeless/CoproductsTest.scala
+++ src/test/scala/learnshapeless/CoproductsTest.scala
@@ -9,4 +9,15 @@ class CoproductsTest extends org.specs2.mutable.Specification with Assert {
 
   eg {mustEqual(ex_nameClue, Coproduct[Clue](name("Florey")))}
 
+  eg {mustEqual(ex_bornClue, Coproduct[Clue](born(1879)))}
+
+  eg {mustEqual(ex_selectName, Option.empty[Name])}
+
+  eg {mustEqual(ex_drop2, Coproduct[Country :+: CNil](australia))}
+
+  eg {mustEqual(Vector(true, false, true, false),
+    shouldCompile("""eg_allClues.map(_.map(ex_isGoodClue))"""))}
+
+  eg {mustEqual(Vector(true, false, true, false), ex_goodClues)}
+
 }
