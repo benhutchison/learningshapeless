7ab0197f397d9a6ed1fad15aeee82814b0c5be3b
Ben Hutchison <brhutchison@gmail.com>
2016-03-05 08:07:16 +1100

    New tag2 operator

--- src/main/scala/learnshapeless/package.scala
+++ src/main/scala/learnshapeless/package.scala
@@ -1,5 +1,16 @@
+import shapeless.tag
+import shapeless.tag.@@
+
 package object learnshapeless {
 
   def assertEquals[T](expected: T, actual: T) = assert(expected == actual, s"Expected: $expected != Actual: $actual")
+
+  object tag2 {
+    def apply[T](t: T) = new TaggedValue[T](t)
+
+    class TaggedValue[T](t: T) {
+      def @@[U]: T @@ U = tag[U].apply[T](t)
+    }
+  }
 
 }
