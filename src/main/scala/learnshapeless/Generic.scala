2a757f1de9b6e1870cf2f6633273f4e310369d7b
Ben Hutchison <brhutchison@gmail.com>
2016-03-17 14:08:28 +1100

    Simpler focused Generic section, new Aux section

--- src/main/scala/learnshapeless/Generic.scala
+++ src/main/scala/learnshapeless/Generic.scala
@@ -1,69 +1,57 @@
 package learnshapeless
 
-import java.time.LocalDate
-
-import learnshapeless.Data.Country
+import learnshapeless.Data._
 import shapeless._
+
+case class PersonDetails(firstName: String, surname: String)
+case class Asset(description: String, value: BigDecimal, categoryCode: String, verified: Boolean)
 
 object GenericRepresentation extends App {
 
   /** Generic provides a powerful set of macros that use compile-time reflection to convert between class hierachies
-    * and a generic form based on HLists and Coproducts
+    * and a generic form based on HLists (aka "products") and Coproducts (aka "sums")
     * */
 
 
   /** Lets start by creating a relatively complex domain model for a load agreement */
 
   def eg_james = PersonDetails("James", "Wong")
-  def eg_office = AddressDetails("11", "High St", "Ivanhoe", Victoria, "3079")
-  def eg_role = "Sales Manager"
 
-  def ex_AuthorizedOfficer: AuthorizedOfficer = ???
+  def eg_genPersonDetails = Generic[PersonDetails]
 
+  def eg_genericJames: String :: String :: HNil = eg_genPersonDetails.to(eg_james)
+
+  def eg_country: Country = Australia
+
+  def eg_genCountry = Generic[Country]
+
+  def eg_genericCountry:  Australia.type :+: England.type :+: Germany.type :+: CNil = eg_genCountry.to(eg_country)
+
+
+
+  /** Product exercise:
+   1. Assign a Generic typeclass for Asset to `ex_genAsset`.
+   2. Use it to convert `eg_asset` to generic form. Specify its explicit type.
+   3. Uncomment and complete the assertion by filling the expected `head` value **/
   def eg_asset = Asset("2012 Toyota Camry", BigDecimal(19500.0), "MOT2", false)
-  def eg_identity = CompanyCertificate("838562067")
-  def eg_borrowerReprentative = PersonDetails("Soula", "Katsopoulis")
-  def eg_address = AddressDetails("1/130", "Low Road", "Errinundra", Victoria, "3079")
 
-  def ex_legal_entity: LegalEntity = ???
+  def ex_genAsset = ???
 
-  def eg_product = MotorVehicleLoan(BigDecimal("16800.00"), 60)
+  def ex_genericAsset = ???
 
-  def ex_loanDetails: LoanDetails = ???
+  //assertEquals(???, ex_genericAsset.head)
 
-  def ex_contract: Contract = ???
+  /** Coproduct exercise:
+    * 1. Assign a Generic typeclass for State to `ex_genState`.
+    * 2. Use it to convert `eg_state` to generic form. Specify its explicit type.
+    * 3. Uncomment and complete the assertion by filling the expected `head` value*/
+  def eg_state: State = Victoria
 
-  /** Use Generic[T] to build a typeclass to operate over the contract */
-  def ex_genericContract = Generic[Contract]
+  def ex_genState = Generic[State]
 
-  def eg_repr = ex_genericContract.to(ex_contract)
+  def ex_genericState: Queensland.type :+: Victoria.type :+: CNil  = ex_genState.to(eg_state)
+
+  //assertEquals(None, ex_genericState.head)
+
 
 }
-case class Contract(borrower: LegalEntity, loadDetails: LoanDetails)
-
-case class LoanDetails(product: Product, officer: AuthorizedOfficer, asset: Asset)
-
-case class AuthorizedOfficer(person: PersonDetails, role: String, officeAddress: AddressDetails)
-
-case class PersonDetails(firstName: String, surname: String)
-
-case class AddressDetails(number: String, street: String, suburb: String, state: State, postcode: String)
-
-sealed trait IdentityDocument
-case class Passport(number: String, dateOfIssue: LocalDate, countryOfIssue: Country) extends IdentityDocument
-case class DriversLicense(number: String, expires: LocalDate, state: State) extends IdentityDocument
-case class CompanyCertificate(abn: String) extends IdentityDocument
-
-sealed trait State
-case object Victoria extends State
-case object Queensland extends State
-
-sealed trait Product
-case class PersonalLoan(amount: BigDecimal, rate: BigDecimal)
-case class MotorVehicleLoan(amount: BigDecimal, termMonths: Int)
-
-case class Asset(description: String, value: BigDecimal, categoryCode: String, verified: Boolean)
-
-sealed trait LegalEntity
-case class Person(details: PersonDetails, identityDocument: IdentityDocument, address: AddressDetails) extends LegalEntity
-case class Company(representative: PersonDetails, identityDocument: IdentityDocument, address: AddressDetails) extends LegalEntity
---
+++ src/main/scala/learnshapeless/WhatIsAux.scala
@@ -0,0 +1,78 @@
+package learnshapeless
+
+import shapeless._
+import shapeless.test.illTyped
+
+object WhatIsAux extends App {
+
+  /** Aux is a recurring design pattern in Shapeless code takes quite a while to get comfortable with.
+    *
+    * This section aims to give you some understanding of the role its playing */
+
+  /** First, observe that MyType.Aux is just a type alias for MyType, with an internal type member converted to a type parameter. */
+
+  val eg_genericAux: Generic.Aux[PersonDetails, String :: String :: HNil] = Generic[PersonDetails]
+
+  /** This is a lossy type ascription */
+  val eg_generic: Generic[PersonDetails] = eg_genericAux
+
+  /** Writing the type as a "refinement" instead of using Aux*/
+  val eg_generic2: Generic[PersonDetails]{type Repr = String :: String :: HNil} = eg_genericAux
+
+  /** The refinement is isomorphic to the Generic.Aux form*/
+  val eg_genericAux2: Generic.Aux[PersonDetails, String :: String :: HNil] = eg_generic2
+
+  /** The type refinement is essential; if lost there's no going back.
+    *
+    * Uncomment this line and compile. Does the error make sense? */
+  //val eg_genericAux2: Generic.Aux[PersonDetails, String :: String :: HNil] = eg_generic
+
+
+
+  def eg_people = Seq(PersonDetails("Zane", "Zoolander"), PersonDetails("James", "Wong"))
+
+
+  /** Converts each element to generic form then returns the head */
+  def headGen[T](ts: Seq[T])(implicit gen: Generic[T]): Option[gen.Repr] = ts.map(gen.to(_)).headOption
+
+
+  /** Apply `headGen` to `eg_people` */
+  def ex_head = ???
+
+
+  /** Enables Ordering over an HList if the element types have an Ordering
+    * Copied from Shapeless examples.*/
+  import GenericOrdering._
+
+  /** Converts each element to generic form then returns the minimum according to the specified Ordering */
+  //def leastGen[T](ts: Seq[T])(implicit gen: Generic[T], o: Ordering[gen.Repr]): gen.Repr = ts.map(gen.to(_)).min
+
+  /** Exercise: Uncomment the above line. What happens? Can you use the info about Aux above to fix it?*/
+
+  /** When you get `leastGen` to compile, apply it to `eg_people` */
+  def ex_least = ???
+
+}
+
+
+trait LowPriorityGenericOrdering {
+  // An Ordering for any type which is isomorphic to an HList, if that HList has an Ordering
+
+  implicit def hlistIsoOrdering[A, H <: HList](implicit gen : Generic.Aux[A, H], oh : Ordering[H]) : Ordering[A] = new Ordering[A] {
+    def compare(a1 : A, a2 : A) = oh.compare(gen to a1, gen to a2)
+  }
+}
+
+object GenericOrdering extends LowPriorityGenericOrdering {
+  implicit def hnilOrdering : Ordering[HNil] = new Ordering[HNil] {
+    def compare(a : HNil, b : HNil) = 0
+  }
+
+  implicit def hlistOrdering[H, T <: HList](implicit oh : Ordering[H], ot : Ordering[T]) : Ordering[H :: T] = new Ordering[H :: T] {
+    def compare(a : H :: T, b : H :: T) = {
+      val i = oh.compare(a.head, b.head)
+      if (i == 0) ot.compare(a.tail, b.tail)
+      else i
+    }
+  }
+}
