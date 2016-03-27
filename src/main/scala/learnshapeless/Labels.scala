2e99c687fcda88c987afed43d4071a895d57cea7
Ben Hutchison <brhutchison@gmail.com>
2016-03-28 09:41:53 +1100

    Solutions for labels module

--- src/main/scala/learnshapeless/Labels.scala
+++ src/main/scala/learnshapeless/Labels.scala
@@ -22,7 +22,7 @@ object LabelsAndRecords extends App {
   def eg_labelledInt = "meaningOfLife" ->> 42
 
   /** Use test.showType() to get the type of above */
-  def ex_typeOfLabelledInt: String = ???
+  def ex_typeOfLabelledInt: String = test.showType(eg_labelledInt)
 
   println(ex_typeOfLabelledInt)
 
@@ -42,34 +42,34 @@ object LabelsAndRecords extends App {
 
   /** Special string indexed accessors are enabled when labelled values are collected in an HList */
   def eg_randomStuff =
-    eg_labelledInt ::
-      ("moonLanding"  ->> LocalDate.of(1969, Month.JULY, 20)) ::
-      ("PiDigits"     ->>  3.14159265359) ::
+    eg_symbolLabelledInt ::
+      ('moonLanding  ->> LocalDate.of(1969, Month.JULY, 20)) ::
+      ('piDigits     ->>  3.14159265359) ::
       HNil
 
-  def eg_moonLanding: LocalDate = eg_randomStuff("moonLanding")
+  def eg_moonLanding: LocalDate = eg_randomStuff('moonLanding)
 
-  def ex_pi: Double = ???
+  def ex_pi: Double = eg_randomStuff('piDigits)
 
 
   /** Use the keys and values methods */
 
-  def ex_randomStuffKeys = ???
+  def ex_randomStuffKeys = eg_randomStuff.keys
 
-  def ex_randomStuffValues = ???
+  def ex_randomStuffValues = eg_randomStuff.values
 
   println(s"keys: $ex_randomStuffKeys   values: $ex_randomStuffValues")
 
   /** Dont trust the NSA man! The moon landing was actually faked in a TV studio 2 weeks earlier.
     * Use +(Key-Value) to redefine the moon landing date to July 4 1969 in eg_randomStuff. */
-  def ex_randomStuffForRealz = ???
+  def ex_randomStuffForRealz = eg_randomStuff + ('moonLanding  ->> LocalDate.of(1969, Month.JULY, 4))
 
   /** Actually, there was no moon landing. Its a synthetic memory implanted when little green men escaped from Roswell
     * Remove the moon landing entry with -(Key) */
-  def ex_reallyRandomStuff = ???
+  def ex_reallyRandomStuff =  eg_randomStuff - 'moonLanding
 
   /** Ensure that its a compile error to access the now non-existent moon landing entry */
-  illTyped("""ex_reallyRandomStuff.<your code here>""")
+  illTyped("""ex_reallyRandomStuff('moonLanding)""")
 
 
   /** Lets move onto LabelledGeneric, a macro powered typeclass for conversion between cases classes and records */
@@ -82,14 +82,14 @@ object LabelsAndRecords extends App {
   def eg_einstein = Scientist("Einstein", 1881, Germany, TheoryOfRelativity)
 
   /** use the `to` method on eg_scientistGen */
-  def ex_genericEinstein = ???
+  def ex_genericEinstein = eg_scientistGen.to(eg_einstein)
   println(s"ex_genericEinstein: $ex_genericEinstein")
 
   /** Oops, that birth year is wrong. Fix it using +(KV) to update ex_genericEinstein with the correct value 1879*/
-  def ex_fixError = ???
+  def ex_fixError = ex_genericEinstein.updated('yearBorn, 1879)
 
   /** Use from the convert back from generic representation to case class */
-  def ex_reconstructedEinstein = ???
+  def ex_reconstructedEinstein = eg_scientistGen.from(ex_fixError)
 
 
 
