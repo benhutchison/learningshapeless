dd1e1237528a06ad1dac0ecfdbd57d38e4ff67f1
Ben Hutchison <brhutchison@gmail.com>
2016-04-04 07:29:01 +1000

    Fix: remove answers from HList master branch

--- src/main/scala/learnshapeless/HLists.scala
+++ src/main/scala/learnshapeless/HLists.scala
@@ -32,17 +32,17 @@ object HLists extends App {
     (a) start the `sbt console`, and enter `:type learnshapeless.HLists.ex_newton`
     (b) if you are using Intellij, you can focus cursor on the expression and activate TypeInfo command (Ctrl-Shift-P on my Mac)
      */
-  def ex_newton = "Newton" :: 1642 :: England :: HNil
+  def ex_newton = ???
 
   import shapeless.syntax.std.tuple._
   def eg_tuple_prepend_append: (String, String, Int, Country, Discovery) =
     ("Albert" +: eg1_einstein).tupled :+ TheoryOfRelativity
 
   /* Exercise 3: Prepend the String "Isaac" to `ex_newton` */
-  def ex_prepend = "Isaac" +: ex_newton
+  def ex_prepend = ???
 
   /* Convert `ex_newton` into a tuple */
-  def ex_tuple: (String, Int, Country) = ex_newton.tupled
+  def ex_tuple: (String, Int, Country) = ???
 
   /* Using operations available via `import syntax.std.tuple._`, append `Calculus` to `ex_tuple`  */
   def ex_tuple_append = ex_tuple :+ Calculus
@@ -50,7 +50,7 @@ object HLists extends App {
   def eg_from_tuple: HList = eg_tuple_prepend_append.productElements
 
   /* convert ex_tuple_append into an HList */
-  def ex_from_tuple = ex_tuple_append.productElements
+  def ex_from_tuple = ???
 
 
   /* Example: Mapping over an HList. Each type `T` in the list should be handled with an `at[T]` expression.
@@ -70,7 +70,7 @@ object HLists extends App {
   object AllCapsPoly extends DefaultIdentityMapping {
     implicit def allCaps = at[String](_.map(_.toUpper))
   }
-  def ex_poly = ex_newton.map(AllCapsPoly)
+  def ex_poly = ???
 
   /* Try writing a Poly1 mapping like `isAustralian` above, that doesn't use a type parameter `C <: Country`. Instead
    * directly define an `at[Country]` clause. Leave other fields unchanged.
@@ -78,13 +78,13 @@ object HLists extends App {
   object IsAustralianPoly extends DefaultIdentityMapping {
     implicit def isAustralia = at[Country](_ == Australia)
   }
-  def ex_poly_country = ex_newton.map(IsAustralianPoly)
+  def ex_poly_country = ???
 
   def eg_index: Country = eg1_einstein(2)
 
   /* Extract the 3rd element of `ex_poly_country` using a index.
   Does the result surprise you? Why did it happen this way? */
-  def ex_poly_country_element_3rd = ex_poly_country(2)
+  def ex_poly_country_element_3rd = ???
   print(ex_poly_country_element_3rd)
 
   def eg_mapped_by_index = eg1_einstein.updateAtWith(2)(_ == Australia)
@@ -92,6 +92,6 @@ object HLists extends App {
   /* Transform the surname field in `ex_newton` to the first name by looking it up in `Data.scientistsFirstNames`.
   * Use updateAtWith to identify the field by numeric index rather than y type.
   * `updateAtWith` returns the old value, and the updated HList in a tuple */
-  def ex_to_firstname_by_index: (String, HList) = ex_newton.updateAtWith(0)(Data.scientistsFirstNames)
+  def ex_to_firstname_by_index: (String, HList) = ???
 
 }
